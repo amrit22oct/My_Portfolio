d53dbdaa83e509df63270abd9dcec4827a025378
amrit22 <amritrai22oct@gmail.com>
2026-03-02 17:36:39 +0530

    change

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 // Render the App component
 root.render(
   <React.StrictMode>
-    <App />
-    {/* <AppNew /> */}
+    {/* <App /> */}
+    <AppNew />
   </React.StrictMode>
 );
